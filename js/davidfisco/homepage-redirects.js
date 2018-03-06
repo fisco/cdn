8d8b0f66e355729ebcbb38585b549b9ebd0dcc7b
David Fisco <dave@davidfisco.com>
2018-03-06 16:09:20 -0500

    Add favicons

---
+++ js/davidfisco/homepage-redirects.js
@@ -0,0 +1,29 @@
+(function() {
+    if (window.location.search == '?q=node/23') {
+        window.location.href = 'http://generalapps.davidfisco.com/synonymical/play';
+    }
+    if (window.location.search == '?q=node/27') {
+        window.location.href = 'http://generalapps.davidfisco.com/dcw';
+    }
+    if (window.location.search == '?q=node/143') {
+        window.location.href = 'http://productivity.davidfisco.com/2007/12/gtd-desktop-wallpaper-version-10.html';
+    }
+    if (window.location.search == '?q=node/109') {
+        window.location.href = 'http://productivity.davidfisco.com/2010/02/simple-calc-workbook-for-gtd-version-11.html';
+    }
+    if (/^https?:\/\/(www\.)?davidfisco\.com\/node\/201/.test(window.location.href)) {
+        window.location.href = 'https://foodie.davidfisco.com/2011/05/canola-oil-and-white-cake.html';
+    }
+    if (/^https?:\/\/(www\.)?davidfisco\.com\/node\/168/.test(window.location.href)) {
+        window.location.href = 'http://software.davidfisco.com/2008/09/experimental-code-lies-ahead-it-is.html';
+    }
+    if (/^https?:\/\/(www\.)?davidfisco\.com\/synonymical\.php/.test(window.location.href)) {
+        window.location.href = 'http://generalapps.davidfisco.com/synonymical/play';
+    }
+    if (/^https?:\/\/(www\.)?davidfisco\.com\/dcwindex\.php/.test(window.location.href)) {
+        window.location.href = 'http://generalapps.davidfisco.com/dcw';
+    }
+    if (/^https?:\/\/(www\.)?davidfisco\.com\/files\/gtd_desktop\.png/.test(window.location.href)) {
+        window.location.href = 'https://productivity.davidfisco.com/2007/12/18/gtd-desktop-wallpaper-version-10.html';
+    }
+})();
