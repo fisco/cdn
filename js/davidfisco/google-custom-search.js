d3ef00d39710202bc0c8ce5ef46c566b981dd59b
David Fisco <dave@davidfisco.com>
2018-03-06 17:21:29 -0500

    Add google-custom-search.js

---
+++ js/davidfisco/google-custom-search.js
@@ -0,0 +1,9 @@
+(function() {
+    var cx = '006375221608361844386:pqxwjqe2kmi';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+})();
